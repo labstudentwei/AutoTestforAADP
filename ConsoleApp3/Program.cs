5809ec919d0a4df6374adbf4cf4efa75afc273d5
labstudentwei <127302964+labstudentwei@users.noreply.github.com>
2024-04-22 21:10:04 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,17 +7,87 @@ using AutoIt;
 using AutoItX3Lib;
 using Microsoft.SqlServer.Server;
 
+
+/*
+ * This is an automation test for the sign-up function. 
+ * * The test simulates: 
+ * * first, the username is empty;
+ * * second, the username already exists; 
+ * * third, the password re-entry is incorrect; 
+ * * fourth, the user successfully signs up.
+ * * 
+ * 
+ * 1.Downlaod AutoIt from https://www.autoitscript.com/site/
+ * 2.Create a new project in Visual Studio and copypaste all the code.
+ * 3.Get nuget package AutoItX.Dotnet.
+ * 4.Add the AutoItX3.dll file to the project reference.
+ * 5.Change the file path in the code to the path of the Management System.exe file.
+ * */
 namespace ConsoleApp3
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            // file path, use\\ instead of \
+            //string filePath = "C:\\Users\\shangwei\\source\\repos\\Testforadd\\bin\\Debug\\Management System.exe";
+            string filePath = "C:\\…………\\Management System.exe";
+
+
+            //Sign-up function test 
+            AutomationTester_SignUp automationTester = new AutomationTester_SignUp(filePath);
+            automationTester.Run();
+
+            //Log-in
+            //AutomationTester_LogIn automationTester = new AutomationTester_LogIn(filePath);
+            //automationTester.Run();
+
+
+
+
+        }
+    }
+
+    public abstract class AutomationTesterBase
+    {
+        protected string filePath;
+
+        public AutomationTesterBase(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        protected void LaunchApplication()
+        {
+            AutoItX.Run(filePath, "", AutoItX.SW_SHOW);
+            AutoItX.WinWaitActive("Management System");
+            AutoItX.Sleep(2000);
+        }
+
+        protected void CloseApplication()
+        {
+            AutoItX.WinClose("Management System");
+        }
+
+        public abstract void Run();
+    }
+
+   
+    public class AutomationTester_SignUp : AutomationTesterBase
+    {
+        public AutomationTester_SignUp(string filePath) : base(filePath) { }
+
+
+        public override void Run()
+        {
+
             Console.WriteLine("This is an automation test for the sign-up function. ");
             Console.WriteLine("The test simulates: ");
-            Console.Write("first, the username already exists; ");
-            Console.Write("second, the password re-entry is incorrect; ");
-            Console.Write("third, the user successfully signs up.\n");
+            Console.Write("first, the username is empty; ");
+            Console.Write("second, the username already exists; ");
+            Console.Write("third, the password re-entry is incorrect; ");
+            Console.Write("fourth, the user successfully signs up.\n");
             Console.Write("Username for test: ");
             string username = Console.ReadLine();
             Console.WriteLine("Automation Test Started.");
@@ -29,11 +99,10 @@ namespace ConsoleApp3
             string phoneNumber = "1234567890";
             string wrongRepassword = "123457";
 
-            // file path, use\\ instead of \
-            string filePath = "C:\\…………\\Management System.exe";
+            LaunchApplication();
 
-            LaunchApplication(filePath);
-
+            //Username is empty
+            TestInsertUserFunction("", password, repassword, phoneNumber);
             //Username already exists
             TestInsertUserFunction(wrongUsername, password, repassword, phoneNumber);
             //Password and RePassword are not the same
@@ -46,21 +115,17 @@ namespace ConsoleApp3
             Console.WriteLine("Automation Test Finished.");
         }
 
-        static void LaunchApplication(string filePath)
-        {
-            AutoItX.Run(filePath, "", AutoItX.SW_SHOW);
-            AutoItX.WinWaitActive("Management System");
-            AutoItX.Sleep(2000);  
-        }
 
-        static void TestInsertUserFunction(string username, string password, string repassword, string phoneNumber)
-        { 
+
+
+        public void TestInsertUserFunction(string username, string password, string repassword, string phoneNumber)
+        {
             if (AutoItX.WinExists("Management System") == 1)
             {
                 string buttonSignUp = "[CLASS:WindowsForms10.BUTTON.app.0.141b42a_r8_ad1; INSTANCE:1]";
                 AutoItX.ControlClick("Management System", "", buttonSignUp);
             }
-            
+
             AutoItX.WinWaitActive("Form1");
 
             string buttonUserName = "[CLASS:WindowsForms10.EDIT.app.0.141b42a_r8_ad1; INSTANCE:4]";
@@ -90,14 +155,46 @@ namespace ConsoleApp3
 
             AutoItX.WinWait("[CLASS:#32770]");
             AutoItX.ControlClick("[CLASS:#32770]", "", buttonConfirm);
-
-
-
         }
-        
-        static void CloseApplication()
-        {
-            AutoItX.WinClose("Management System");
-        }
+
+
     }
+
+    public class AutomationTester_LogIn : AutomationTesterBase
+    {
+        public AutomationTester_LogIn(string filePath) : base(filePath) { }
+
+        public override void Run()
+        {
+            Console.WriteLine("Automation Test Started.");
+            LaunchApplication();
+            LogIn();
+            AutoItX.WinWaitActive("Main Page");
+            AutoItX.Sleep(2000);
+
+            //CloseApplication();
+            //Console.WriteLine("Automation Test Finished.");
+        }
+
+        public void LogIn()
+        {
+            string userName = "[CLASS:WindowsForms10.EDIT.app.0.141b42a_r8_ad1; INSTANCE:2]";
+            string password = "[CLASS:WindowsForms10.EDIT.app.0.141b42a_r8_ad1; INSTANCE:1]";
+            string buttonLogIn = "[CLASS:WindowsForms10.BUTTON.app.0.141b42a_r8_ad1; INSTANCE:2]";
+            string buttonConfirm = "[CLASS:Button; INSTANCE:1]";
+            AutoItX.ControlClick("Management System", "", userName);
+            AutoItX.ControlSetText("Management System", "", userName, "admin");
+            AutoItX.ControlClick("Management System", "", password);
+            AutoItX.ControlSetText("Management System", "", password, "admin");
+            AutoItX.ControlClick("Management System", "", buttonLogIn);
+            AutoItX.WinWait("[CLASS:#32770]");
+            AutoItX.ControlClick("[CLASS:#32770]", "", buttonConfirm);
+
+            AutoItX.Sleep(3000);
+        }
+
+
+    }
+
+
 }
